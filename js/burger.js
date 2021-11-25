53cbee62e8c691be35cf160f9a3b44f4fe966b69
Kromus22 <kromus2@gmail.com>
2021-11-25 21:26:33 +0500

    +

---
+++ js/burger.js
@@ -0,0 +1,12 @@
+const burgerMenu = (selectorBurger, selectorMenu, burgerActive, menuActive) => {
+  const burger = document.querySelector(selectorBurger)
+
+  const menu = document.querySelector(selectorMenu)
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle(burgerActive)
+    menu.classList.toggle(menuActive)
+  })
+}
+
+export default burgerMenu;
